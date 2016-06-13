da0b0ef4bf26a080f77ee859a428979ebee12a29
RamonHossein <hosseinramon@gmail.com>
2016-06-13 16:19:11 -0300

    pack countries 01

--- LIST.md
+++ LIST.md
@@ -2,11 +2,21 @@
 
 | Country | Identification | Format | Link |
 |:-------:|:------------:|:-----------:|:-----------:|
+| Algeria | DZ | 16027 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/dzaEn.pdf) |
+| Australia | AU | 3171 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/ausEn.pdf) |
+| Austria | AT | 1220 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/autEn.pdf) |
 | Brazil | BR | 35001-345 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/braEn.pdf) |
 | China | CN | 200082 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/chnEn.pdf) |
-| Germany or Deutsch | DE | 81545 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/deuEn.pdf) |
+| Denmark | DK | 8660 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/dnkEn.pdf) |
+| Deutsch or Germany | DE | 81545 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/deuEn.pdf) |
+| Estonia | EE | 69501 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/estEn.pdf) |
+| Finland | FI | 50100 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/finEn.pdf) |
 | France | FR | 75007 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/fraEn.pdf) |
+| Indonesia | ID | 40198 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/idnEn.pdf) |
+| Israel | IL | 94142 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/isrEn.pdf) |
 | Italy | IT | 20090 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/itaEn.pdf) |
 | Japan | JP | 208–0032 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/jpnEn.pdf) |
+| Kazakhstan | KZ | 040900 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/kazEn.pdf) |
+| Singapore | SG | 546080 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/sgpEn.pdf) |
 | Spain | ES | 28001 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/espEn.pdf) |
 | United States | US | 90410 or 10118-0229 | [documentation](http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/usaEn.pdf) |
--- lib/validate_zipcode/regex.rb
+++ lib/validate_zipcode/regex.rb
@@ -1,42 +1,93 @@
 module ValidateZipcode
   module Regex
-    def self.US(zipcode)
-      match = zipcode =~ /^([0-9]{5})$|([0-9]{9})$|([0-9]{5}-[0-9]{4})$/
+    # Algeria
+    def self.DZ(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
       match == 0 ? true : false
     end
-
+    # Australia
+    def self.AU(zipcode)
+      match = zipcode =~ /^([0-9]{4})$/
+      match == 0 ? true : false
+    end
+    # Austria
+    def self.AT(zipcode)
+      match = zipcode =~ /^([0-9]{4})$/
+      match == 0 ? true : false
+    end
+    # Brazil
     def self.BR(zipcode)
       match = zipcode =~ /^([0-9]{5}-[0-9]{3})$|([0-9]{8})$/
       match == 0 ? true : false
     end
-
-    def self.FR(zipcode)
-      match = zipcode =~ /^([0-9]{5})$/
-      match == 0 ? true : false
-    end
-
-    def self.IT(zipcode)
-      match = zipcode =~ /^([0-9]{5})$/
-      match == 0 ? true : false
-    end
-
-    def self.DE(zipcode)
-      match = zipcode =~ /^([0-9]{5})$/
-      match == 0 ? true : false
-    end
-
-    def self.ES(zipcode)
-      match = zipcode =~ /^([0-9]{5})$/
-      match == 0 ? true : false
-    end
-
+    # China
     def self.CN(zipcode)
       match = zipcode =~ /^([0-9]{6})$/
       match == 0 ? true : false
     end
-
+    # Denmark
+    def self.DK(zipcode)
+      match = zipcode =~ /^([0-9]{4})$/
+      match == 0 ? true : false
+    end
+    # Deutsch or Germany
+    def self.DE(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Estonia
+    def self.EE(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Finland
+    def self.FI(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # France
+    def self.FR(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Indonesia
+    def self.ID(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Israel
+    def self.IL(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Italy
+    def self.IT(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Japan
     def self.JP(zipcode)
       match = zipcode =~ /^([0-9]{3}-[0-9]{4})$|([0-9]{7})$/
+      match == 0 ? true : false
+    end
+    # Kazakhstan
+    def self.KZ(zipcode)
+      match = zipcode =~ /^([0-9]{6})$/
+      match == 0 ? true : false
+    end
+    # Singapore
+    def self.SG(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # Spain
+    def self.ES(zipcode)
+      match = zipcode =~ /^([0-9]{5})$/
+      match == 0 ? true : false
+    end
+    # United Stade
+    def self.US(zipcode)
+      match = zipcode =~ /^([0-9]{5})$|([0-9]{9})$|([0-9]{5}-[0-9]{4})$/
       match == 0 ? true : false
     end
   end
